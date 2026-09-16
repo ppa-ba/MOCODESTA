431fc4cf2ca8330fb7ec82727ae04ea2697c3722
ppa-ba <134120587+ppa-ba@users.noreply.github.com>
2026-09-17 00:24:15 +0700

    login.js

--- login.js
+++ login.js
@@ -2,8 +2,8 @@ function validate(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     if ( username == "mocodesta" && password == "rahmat"){
-    alert ("SELAMAT DATANG DI APLIKASI MOCODESTA NEXT GENERATION, SILAHKAN KLIK UNTUK MASUK KE MENU UTAMA");
-    window.location = "menu.html"; // Redirecting to other page.
+    alert ("SELAMAT DATANG DI APLIKASI MOCODESTA, SILAHKAN KLIK UNTUK MASUK KE DASHBOARD UTAMA");
+    window.location = "dashboard.html"; // Redirecting to other page.
     return false;
     }
     else{
