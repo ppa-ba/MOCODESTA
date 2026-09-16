de396df03617e124f1a804c4d033ba4204c451de
ppa-ba <134120587+ppa-ba@users.noreply.github.com>
2026-09-17 00:05:44 +0700

    Add files via upload

---
+++ data/DRONEVIEW_13.js
@@ -0,0 +1,28 @@
+var json_DRONEVIEW_13 = {"type":"FeatureCollection","name":"DRONEVIEW_13","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[
+    {"type":"Feature","properties":{"DRONE VIEW":"AREA HW TR",
+    "EASTING":369082,
+    "NORTHING":9580956,
+    "TAKEN DATE":"230716-BY MUHAMMAD RABBIN ARRAFAT",
+    "ATTCH PICT":"../PICTURES/TR PNG.png"},
+    "geometry":{"type":"Point","coordinates":[103.82099410,-3.79036137]}},
+    
+    {"type":"Feature","properties":{"DRONE VIEW":"AREA HW SAWIT ALI",
+    "EASTING":368883.1111,
+    "NORTHING":9581092.111,
+    "TAKEN DATE":"230716-BY MUHAMMAD RABBIN ARRAFAT",
+    "ATTCH PICT":"../PICTURES/SA PNG.png"},
+    "geometry":{"type":"Point","coordinates":[103.819204898047857,-3.789127779368231]}},
+    
+    {"type":"Feature","properties":{"DRONE VIEW":"AREA LW CSA PIT 2",
+    "EASTING":369537,
+    "NORTHING":9581223,
+    "TAKEN DATE":"230716-BY MUHAMMAD RABBIN ARRAFAT",
+    "ATTCH PICT":"../PICTURES/LW PNG.png"},
+    "geometry":{"type":"Point","coordinates":[103.82509436,-3.78795194]}},
+    
+    {"type":"Feature","properties":{"DRONE VIEW":"AREA ALL PIT 2",
+    "EASTING":36907,
+    "NORTHING":9580724,
+    "TAKEN DATE":"230716-BY MUHAMMAD RABBIN ARRAFAT",
+    "ATTCH PICT":"../PICTURES/ALLPITU PNG.png"},
+    "geometry":{"type":"Point","coordinates":[103.82571858,-3.79246626]}}]}
---
+++ data/PRASARANA_14.js
@@ -0,0 +1,35 @@
+var json_PRASARANA_14 = {"type":"FeatureCollection","name":"PRASARANA_14","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[
+    {"type":"Feature","properties":{"PRASARANA":"MAIN OFFICE PPA",
+    "EASTING":371368.6014,
+    "NORTHING":9582768.032},
+    "geometry":{"type":"Point","coordinates":[103.841605408989309,-3.773999310100701]}},
+    
+    {"type":"Feature","properties":{"PRASARANA":"WORKSHOP PPA",
+    "EASTING":371244.6981,
+    "NORTHING":9582773.485},
+    "geometry":{"type":"Point","coordinates":[103.840489812799262,-3.773948495092392]}},
+    
+    {"type":"Feature","properties":{"PRASARANA":"CSA PIT 1",
+    "EASTING":371186.1495,
+    "NORTHING":9582418.433},
+    "geometry":{"type":"Point","coordinates":[103.839958361425971,-3.777159253653561]}},
+    
+    {"type":"Feature","properties":{"PRASARANA":"CSA PIT 2",
+    "EASTING":369300.1456,
+    "NORTHING":9580549.431},
+    "geometry":{"type":"Point","coordinates":[103.822953397906673,-3.794041446943319]}},
+    
+    {"type":"Feature","properties":{"PRASARANA":"PPA RESIDENCE",
+    "EASTING":368665.590,
+    "NORTHING":9585863.395},
+    "geometry":{"type":"Point","coordinates":[103.81730454,-3.74596894]}},
+
+    {"type":"Feature","properties":{"PRASARANA":"OFFICE GA PPA BA",
+    "EASTING":367163.590,
+    "NORTHING":9584605.395},
+    "geometry":{"type":"Point","coordinates":[103.803785,-3.757298]}},
+    
+    {"type":"Feature","properties":{"PRASARANA":"PITSTOP",
+    "EASTING":370311.6021,
+    "NORTHING":9581409.965},
+    "geometry":{"type":"Point","coordinates":[103.8320714561344,-3.786270285614753]}}]}
